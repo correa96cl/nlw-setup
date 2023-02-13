421aa639e7244b1b255b2da3c2a0a378ec121f36
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2023-02-12 22:09:01 -0300

    Primeiro commit do novo NLW

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+import './styles/global.css'
+import './App.css'
+import { Habit } from './components/Habit'
+
+function App() {
+
+
+  return (
+    <div>
+
+
+      <Habit completed={3} />
+
+    </div>
+  )
+}
+
+export default App
