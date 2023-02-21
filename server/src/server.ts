d5fe4327b9ce8f64fd9588bbe44dcb84c08e5c8e
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2023-02-20 21:08:11 -0300

    Commit da segunda aula

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import { appRoutes } from './routes';
 
 const app = Fastify();
-const prisma = new PrismaClient();
+
 
 app.register(cors)
+app.register(appRoutes);
 
-app.get('/hello', async () => {
-   const habits = await prisma.habit.findMany({
-    where: {
-        title: {
-            startsWith: 'Beber'
-        }
-    }
-   })
-
-   return habits
-})
 
 app.listen({
     port: 3333,
